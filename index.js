832810ce053df0c4c680ab05464fc5c91ff49313
Júlio César <77865836+xarazinho-user@users.noreply.github.com>
2024-11-29 18:28:15 -0300

    .

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 const express = require('express');
 const cors = require('cors');
-const { Builder } = require('xml2js');
-const xml2js = require('xml2js');
+const { parseString, Builder } = require('xml2js');
 const bodyParser = require('body-parser');
 
 const port = 51492;
 
-const parser = new xml2js.Parser({ ignoreAttrs: true });
-
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.text({ type: '/', limit: '50mb' }));
+app.use(bodyParser.text({ type: '*/*', limit: '50mb' }));
 
 app.use((req, res, next) => {
   if (!req.body) {
-    throw new Error('Requisição sem conteúdo');
+    throw new Error('Requisi��o sem conte�do');
   }
   next();
 });
@@ -31,7 +28,7 @@ app.post('/json-to-xml', (req, res) => {
 
 app.post('/xml-to-json', (req, res) => {
   try {
-    parser.parseString(req.body, { explicitArray: false }, (error, result) => {
+    parseString(req.body, { explicitArray: false, ignoreAttrs: true }, (error, result) => {
       res.status(200).send(result);
     });
   } catch (error) {
